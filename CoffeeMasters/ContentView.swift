46a5f336420a53ebb56c6765aa563ab347f954a5
pasDamola <oyindadavid18@gmail.com>
2023-09-18 08:04:24 +0100

    Offers page

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -9,11 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Greeting()
-            Greeting()
-            Greeting()
-        }
+        OffersPage()
        
     }
 }
